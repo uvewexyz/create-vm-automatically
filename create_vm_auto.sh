aa8cadebcf4ac633f75a3b2c8a9024ebc6673b1a
kingz-adit <muhammadaditiyarakhman@gmail.com>
2025-06-05 23:20:23 +0700

    Update script create_vm_auto.sh v3.1.0:
    Descriptions:
    - Improve script by adding function to some condition fields
    - Validate the ip addr segmen in the input ip addr section
    - Make easy workflows the script with the line break, example, and guide to fill the question at run the script

--- create_vm_auto.sh
+++ create_vm_auto.sh
@@ -11,15 +11,16 @@ centosstream9="centos-stream9-latest.img"
 openwrt="openwrt-24.10.1-x86-generic-ext4-combined.img"
 red="\033[0;31m"
 reset="\033[0m"
+line="-------------------------------------------------------------------------------------"
 
 # Declare arrays to keep the virtual network names and interfaces
 declare -a net_name
 declare -a net_if
 
 # Start of the script
-echo "-------------------------------------------------------------------------------------"
+echo "$line"
 read -n 2 -e -i "Y" -p "Welcome, Do you want to create a VM? (Y/n, default Y): " response;
-echo "-------------------------------------------------------------------------------------"
+echo "$line"
 sleep 1;
 
 if [[ "$response" != "Y" && "$response" != "y" ]]; then
@@ -27,35 +28,84 @@ if [[ "$response" != "Y" && "$response" != "y" ]]; then
   exit 1;
 fi
 
-# Prompt to specify th VM name
-read -e -i "my-vm$(date +%d_%m_%y)" -p "Create the VM name: " vm_name;
-echo "-------------------------------------------------------------------------------------"
-
 # Validate if the VM name already exists
-if virsh list --all --name | grep -qwF -- "$vm_name"; then
-  echo "This name is already use, exit script...";
-  echo "-------------------------------------------------------------------------------------"
-  exit 1;
-fi
+valid_name() {
+  # Prompt to specify th VM name
+  read -e -i "vm$(date +%d_%m_%y)" -p "Create the VM name: " vm_name;
+  echo "$line"
 
-# Prompt to specify thre VM memory size
-read -e -i "512" -p "Size memory to allocate for the VM, in MiB: " vm_mem;
-echo "-------------------------------------------------------------------------------------"
+  if virsh list --all --name | grep -qwF -- "$vm_name"; then
+    echo "This name is already use, exit script...";
+    sleep 3;
+    clear && valid_name;
+  else
+    echo -e "Keep vm name: $red $vm_name $reset";
+    echo "$line"
+  fi
+}
 
-# Prompt to specify the VM vCPU size
-read -e -i "1" -p "Size of virtual cpus for the VM: " vm_vcpu;
-echo "-------------------------------------------------------------------------------------"
+valid_name;
 
-# Prompt to select the OS for the VM
-echo -e "
-1.) ubuntu22.04
-2.) ubuntu20.04
-3.) almalinux9
-4.) centos-stream9
-5.) openwrt
-"
-read -e -n 2 -p "Type OS number for your VM OS: " vm_os;
-echo "-------------------------------------------------------------------------------------"
+# Validate the VM memory size
+valid_mem() {
+  # Prompt to specify thre VM memory size
+  vm_mem_max="$(grep -i "MemAvailable" /proc/meminfo | awk '{print $2/1048}' | cut -d. -f1)"
+  echo -e "Minimal input: $red 128 $reset MiB";
+  echo -e "Maximal input: $red $vm_mem_max $reset MiB";
+  read -e -p "Specify size memory to allocate for the VM, in MiB: " vm_mem;
+  echo "$line"
+
+  if [[ "$vm_mem" -lt 128 || "$vm_mem" -ge "$vm_mem_max" ]]; then
+    echo "Invalid memory size! Enter value between $red 128 $reset MiB - $red $vm_mem_max $reset MiB";
+    echo "$line";
+    sleep 3;
+    clear && valid_mem;
+  else
+    echo -e "Keep vm memory size: $red $vm_mem $reset MiB";
+    echo "$line";
+  fi
+}
+
+valid_mem;
+
+# Validate allocating the VM vCPU
+valid_vcpu() {
+  # Prompt to specify the VM vCPU size
+  echo -e "Minimal input: $red 1 $reset vCPU";
+  echo -e "Maximal input: $red $(nproc) $reset vCPU";
+  read -e -i "1" -p "Size of virtual cpus for the VM: " vm_vcpu;
+  echo "$line"
+
+  if [[ "$vm_vcpu" -lt 1  || "$vm_vcpu" -ge "$(nproc)" ]]; then
+    echo "Invalid vCPU size! Enter value between $red 1 $reset vCPU - $red $(nproc) $reset vCPU";
+    echo "$line";
+    sleep 3;
+    clear && valid_vcpu;
+  else
+    echo -e "Keep vm vCPU size: $red $vm_vcpu $reset vCPU";
+    echo "$line";
+  fi
+}
+
+valid_vcpu;
+
+# Validate the OS VM
+valid_os() {
+  # Prompt to select the OS for the VM
+  echo -e "
+  1.) ubuntu22.04
+  2.) ubuntu20.04
+  3.) almalinux9
+  4.) centos-stream9
+  5.) openwrt
+  "
+
+  echo "Example selection: 1, 2, 3, 4, or 5";
+  read -e -p "Type OS number for your VM OS: " vm_os;
+  echo "$line"
+}
+
+valid_os;
 
 # Prompt to specify the size of the primary disk
 read -e -i "15" -p "Specify size for the primary disk image in Gigabyte: " vm_disk1_size;
@@ -93,7 +143,7 @@ esac
 
 # Show the primary disk image path
 echo "Primary disk image path: $vm_disk1";
-echo "-------------------------------------------------------------------------------------"
+echo "$line"
 
 case "$vm_os" in
   1)
@@ -113,76 +163,107 @@ case "$vm_os" in
     ;;
   *)
     echo "Option not found!. Please select 1, 2, 3, 4, or 5!!!"
-    exit 1
+    sleep 3;
+    clear && valid_os;
     ;;
 esac
+# esac
 
 # Prompt to specify the size of the secondary disk
 read -e -i "5" -p "Specify size for a new secondary disk image in Gigabyte: " vm_disk2_size;
-echo "-------------------------------------------------------------------------------------"
+echo "$line"
 
-# Populate the arrays with values from the net var and the iface var
-for net in $(virsh net-list --all --name); do
-  iface=$(virsh net-dumpxml $net | awk -F"'" '/bridge name=/{print $2}');
-  net_name+=("$net")
-  net_if+=("$iface")
-done
+# Validate attached virtual network to the VM 
+valid_vir_net() {
+  # Populate the arrays with values from the net var and the iface var
+  for net in $(virsh net-list --all --name); do
+    iface=$(virsh net-dumpxml $net | awk -F"'" '/bridge name=/{print $2}');
+    net_name+=("$net")
+    net_if+=("$iface")
+  done
 
-# Check if user haven't any virtual networks
-if [[ ${#net_name[@]} -eq 0 ]]; then
-  echo "No virtual networks found. Please create a virtual network first.";
-  echo "Exiting script...";
+  # Check if user haven't any virtual networks
+  if [[ ${#net_name[@]} -eq 0 ]]; then
+    echo "No virtual networks found. Please create a virtual network first.";
+    echo "Exiting script...";
+    sleep 1;
+    exit 1;
+  fi
+
+  # List available virtual networks
+  echo "Available virtual networks:";
   sleep 1;
-  exit 1;
-fi
+  for i in "${!net_name[@]}"; do
+    num=$((i + 1));
+    echo -e "$num.) Virtual Network: $red ${net_name[$i]} $reset | Interface: $red ${net_if[$i]} $reset";
+  done
 
-# List available virtual networks
-echo "Available virtual networks:";
-sleep 1;
-for i in "${!net_name[@]}"; do
-  num=$((i + 1));
-  echo -e "$num.) Virtual Network: $red ${net_name[$i]} $reset | Interface: $red ${net_if[$i]} $reset";
-done
+  # Prompt to select a virtual network
+  echo "Example selection: 1, 2, 3, or etc";
+  read -e -p "Select the number of the virtual network to attach to the VM: " net_num;
+  echo "$line"
+  sleep 1;
 
-# Prompt to select a virtual network
-read -e -n 2 -p "Select the number of the virtual network to attach to the VM: " net_num;
-echo "-------------------------------------------------------------------------------------"
-sleep 1;
+  # Validate the selected virtual network number
+  if [[ "$net_num" =~ ^[0-9]+$ && "$net_num" -gt 0 && "$net_num" -le "${#net_name[@]}" ]]; then
+    vm_net_select="${net_name[$((net_num - 1))]}";
+    vm_if_select="${net_if[$net_num - 1]}";
+    ip_gw="$(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.1\/24/g')";
+    ip_start="$(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.2\/24/g')";
+    ip_end="$(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.254\/24/g')";
+    echo -e "Selecting Virtual Network: $red $vm_net_select $reset | Interface: $red $vm_if_select $reset";
+    echo "$line"
+    sleep 3;
+    echo -e "Your gateway: $red $ip_gw $reset";
+    echo -e "Your ip address start: $red $ip_start $reset";
+    echo -e "Your ip address end: $red $ip_end $reset";
+    echo -e "Example IP address: $red $(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.10\/24/g') $reset";
+    echo "$line"
+  else
+    echo "Invalid selection. Please select a valid number from the list.";
+    sleep 3;
+    clear && valid_vir_net;
+  fi
+}
 
-# Validate the selected virtual network number
-if [[ "$net_num" =~ ^[0-9]+$ && "$net_num" -gt 0 && "$net_num" -le "${#net_name[@]}" ]]; then
-  vm_net_select="${net_name[$((net_num - 1))]}";
-  vm_if_select="${net_if[$net_num - 1]}";
-  ip_gw="$(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.1\/24/g')";
-  echo -e "Selecting Virtual Network: $red $vm_net_select $reset | Interface: $red $vm_if_select $reset";
-  echo "-------------------------------------------------------------------------------------"
-  sleep 3;
-  echo -e "Your gateway: $red $ip_gw $reset";
-  echo -e "Your ip address start with: $red $(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.2\/24/g') $reset";
-  echo -e "Your ip address start with: $red $(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.254\/24/g') $reset";
-  echo -e "Example IP address: $red $(ip addr show $vm_if_select | awk 'NR == 3 {print $2}' | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+\/24/\1.10\/24/g') $reset";
-  echo "-------------------------------------------------------------------------------------"
-else
-  echo "Invalid selection. Please select a valid number from the list.";
-  exit 1;
-fi
+valid_vir_net;
 
-# Prompt to assign a ip
-read -e -p "Assign the ip address to the VM: " ip_num;
-echo "-------------------------------------------------------------------------------------"
+# Validate the IP address
+valid_ip() {
+  ip_addr="${ip_gw%%/*}"
+  ip_net="$(ipcalc -n "$ip_gw" | awk -F: '/Network/ {gsub(/ /, "", $2); print $2}' | cut -d/ -f1)"
+  netmask="$(ipcalc "$ip_gw" | awk -F: '/Netmask/ {gsub(/ /, "", $2); print $2}' | cut -d= -f1)"
+
+  # Prompt to assign a ip
+  read -e -p "Assign the ip address to the VM: " ip_num;
+  echo "$line"
+
+  ip_input=$(ipcalc -n "$ip_num" "$netmask" | awk -F: '/Network/ {gsub(/ /, "", $2); print $2}' | cut -d/ -f1)
+
+  if [[ "$ip_input" != "$ip_net" ]]; then
+    echo "IP $ip_num not in subnet $ip_net! Please enter again";
+    sleep 3;
+    clear && valid_ip;
+  else
+    echo -e "Keep ip address: $red $ip_num $reset";
+    echo "$line"
+  fi
+}
+
+valid_ip;
 
 # Prompt to create a new user for the VM
 read -e -i "john" -p "Create a new user for the VM: " vm_user;
-echo "-------------------------------------------------------------------------------------";
+echo "$line";
 
 # Prompt to create a password for new user
 read -e -p "Create a password for the user: " vm_passwd;
 secret="$(echo "$vm_passwd" | mkpasswd -s --method=SHA-512 --rounds=500000)"
-echo "-------------------------------------------------------------------------------------";
+echo "$line";
 
 # Prompt to add public key SSH
-read -e -i "$(cat ./key/id_ed25519.pub)" -p "Add your pub key to the VM: " vm_pubkey;
-echo "-------------------------------------------------------------------------------------";
+read -e -i "$(cat ~/.ssh/id_ed25519.pub)" -p "Add your pub key to the VM: " vm_pubkey;
+echo "$line";
 
 # Load config to user-data file
 cat << EOF > "$dst_dir"/"$vm_name"-user-data
@@ -220,24 +301,27 @@ write_files:
               - to: default
                 via: $ip_gw
 
-runcmd:
-  - netplan apply
-  - echo "AllowUsers $vm_user" >> /etc/ssh/sshd_config
-  - sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-  - systemctl restart sshd
-  - cloud-init status --wait
-
 # Update, upgrade, and install packages
 package_upgrade: true
 package_update: true
 packages:
 - neofetch
 - vim
+- nginx
+
+runcmd:
+  - netplan apply
+  - echo "AllowUsers $vm_user" >> /etc/ssh/sshd_config
+  - sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+  - systemctl restart sshd
+  - chmod 600 /etc/netplan/99-custom-network.yaml
+  - systemctl enable nginx && systemctl start nginx
+  - cloud-init status --wait
 EOF
 
 # Process the VM creation
 echo -e "Create VM name $red $vm_name $reset";
-echo "-------------------------------------------------------------------------------------"
+echo "$line"
 sleep 2;
 virt-install -q -n "$vm_name" \
   --memory "$vm_mem" \
@@ -256,7 +340,7 @@ echo "Result: ";
 # Validate if the VM is already created
 if virsh list --all --name | grep -qwF -- "$vm_name"; then
   echo -e "Successfully created VM with name $red $vm_name $reset";
-  echo "-------------------------------------------------------------------------------------"
+  echo "$line"
   virsh list --all | grep -i "$vm_name";
 else
   echo "VM failed to create! There was an error during the process.";
