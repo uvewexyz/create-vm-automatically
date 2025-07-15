a83d41b063bd54b5eb4e5be85f31a0de64a861ca
kingz-adit <muhammadaditiyarakhman@gmail.com>
2025-07-15 20:59:08 +0700

    Update script create_vm_auto.sh v3.5.1

--- create_vm_auto.sh
+++ create_vm_auto.sh
@@ -312,19 +312,29 @@ valid_primary_disk() {
   # Begin to match OS with available pattern
   osinfo=($(osinfo-query os --fields=short-id,name,codename | grep -i "${patterns[0]}" | awk '{print $1}'));
   osfinal=();
-  for pattern in "${patterns[@]}"; do
-    if [[ "${#osinfo[@]}" != 1 ]]; then
-      osfinal+=($(printf '%s\n' "${osinfo[@]}" | grep -i "${pattern}"));
-      echo "Pattern 0 & 1"
-      echo -e "${blue}INFO:${reset} ${red}${osfinal[@]}${reset}";
-      break;
-    else
-      osfinal=("${osinfo[@]}");
-      echo "Pattern 0"
+  i=0;
+
+  while [[ $i -lt "${#patterns[@]}" ]]; do
+    osfinal+=($(printf '%s\n' "${osinfo[@]}" | grep -i "${patterns[$i]}"));
+    if [[ "${#osfinal[@]}" == 1 ]]; then
       echo -e "${blue}INFO:${reset} ${red}${osfinal[@]}${reset}";
       break;
     fi
+    ((i++));
   done
+  # for pattern in "${patterns[@]}"; do
+    # if [[ "${#osinfo[@]}" != 1 ]]; then
+      # osfinal+=($(printf '%s\n' "${osinfo[@]}" | grep -i "${pattern}"));
+      # echo "Pattern 0 & 1"
+      # echo -e "${blue}INFO:${reset} ${red}${osfinal[@]}${reset}";
+      # break;
+    # else
+      # osfinal=("${osinfo[@]}");
+      # echo "Pattern 0"
+      # echo -e "${blue}INFO:${reset} ${red}${osfinal[@]}${reset}";
+      # break;
+    # fi
+  # done
 }
 
 valid_primary_disk;
